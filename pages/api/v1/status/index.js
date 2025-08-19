1580a4c43b489e1855d835e981e65f7ee860d69b
Gustavo Weinschütz <gpweins@gmail.com>
2025-08-19 01:36:34 +0000

    Add `database.js` and environment configurations

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("Select 1 + 1 as sum;");
   response.status(200).json({ chave: "valor" });
 }
 
